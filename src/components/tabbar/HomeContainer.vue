84c7b91ce016582c495956007bfd2f665acdaa03
liuyuanyuan <1445179059@qq.com>
2018-12-29 13:15:22 +0800

    modified:   package.json 	modified:   src/components/tabbar/HomeContainer.vue 	modified:   src/main.js 	modified:   src/components/tabbar/HomeContainer.vue

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,12 +1,45 @@
 <template>
     <div>
-        <h1>home</h1>
+        <h1 style="display: none;">home</h1>
+        <!--轮播图区域-->
+        <mt-swipe :auto="4000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
     </div>
 </template>
 <script>
-
+  export default{
+    data(){
+      return {}
+    },
+    methods: {
+      //轮播图
+      carousel(){
+        
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-
+    .mint-swipe {
+        height: 200px;
+        .mint-swipe-item {
+            /*sass交替选择器，并且嵌套*/
+            &:nth-of-type(1) {
+                background-color: red;
+            }
+            &:nth-of-type(2) {
+                background-color: green;
+            }
+            &:nth-of-type(3) {
+                background-color: yellow;
+            }
+        }
+        /*.mint-swipe-item:nth-of-type(2) /:nth-child(2){
+            background-color: red;
+        }*/
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -14,8 +14,10 @@ import './index.css'
 import Vue from 'vue'
 import app from './app.vue'
 // 导入 table 和 分页组件
-import {VTable,VPagination} from 'vue-easytable'
-
+import {VTable, VPagination} from 'vue-easytable'
+//导入vue-resource
+import  VueResource from 'vue-resource'
+Vue.use(VueResource);
 
 //路由模块
 import VueRouter from 'vue-router'
@@ -23,7 +25,7 @@ import router from '../routes/router'
 Vue.use(VueRouter);
 
 //导入Mint UI--基于 Vue.js 的移动端组件库
-import MintUI from 'mint-ui'
+import MintUI from 'mint-ui'    //样式文件需要单独引入。
 Vue.use(MintUI)
 import V2Table from 'v2-table';
 Vue.use(V2Table)
@@ -31,7 +33,6 @@ Vue.use(V2Table)
 //导入js
 import 'vue-easytable/umd/js/index'
 import test, {title, content} from './test.js'
-
 
 
 // 注册到全局
