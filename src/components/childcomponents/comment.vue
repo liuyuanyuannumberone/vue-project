91b8293abaf5cae8affd75453038e30cf3134040
liuyuanyuan <1445179059@qq.com>
2018-12-31 23:57:19 +0800

    new file:   src/components/childcomponents/comment.vue 	modified:   src/components/news/NewsInfo.vue 	new file:   static/html.html 	deleted:    "static/\344\270\223\351\227\250\346\265\213\350\257\225\344\273\243\347\240\201.html"

---
+++ src/components/childcomponents/comment.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="comment">
+        <h3>发表评论</h3>
+        <hr>
+        <textarea placeholder="发表评论(最多120字)" maxlength="120">
+
+        </textarea>
+        <mt-button type="primary" size="large">发表评论</mt-button>
+    </div>
+</template>
+
+<script>
+
+  export default{
+    data(){
+      return {}
+    },
+    created(){
+
+    },
+    methods: {},
+  }
+</script>
+
+<style lang="scss" scoped>
+    .comment {
+        h3{
+            margin-top: 34px;
+        }
+        hr {
+            margin: 0;
+        }
+        textarea{
+            margin-bottom:10px;
+        }
+    }
+</style>
